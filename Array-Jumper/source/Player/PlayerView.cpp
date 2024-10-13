65f9d6be426e67ba6fe1344544ce6936dcb73d01
Adnan Sayyed <adnan.sayyed.1947@gmail.com>
2024-10-13 18:58:09 +0530

    created code to render player

---
+++ Array-Jumper/source/Player/PlayerView.cpp
@@ -0,0 +1,68 @@
+#include "../../header/Player/PlayerView.h"
+#include "../../header/Global/ServiceLocator.h"
+#include "../../header/Global/Config.h"
+
+namespace Player
+{
+    using namespace Global;
+    PlayerView::PlayerView()
+    {
+        game_window = nullptr;
+        player_image = new UI::UIElement::ImageView();
+    }
+
+    PlayerView::~PlayerView() {}
+
+
+    void PlayerView::update()
+    {
+        // Yet to implement
+    }
+
+    void PlayerView::render()
+    {
+        // Yet to implement
+    }
+
+    void PlayerView::calculatePlayerDimensions()
+    {
+        player_height = 1000.f;
+        player_width = 1000.f;
+    }
+
+    void PlayerView::initializePlayerImage()
+    {
+        player_image->initialize(Config::character_texture_path,
+                                 player_width,
+                                 player_height,
+                                 sf::Vector2f(0, 0));
+    }
+
+    void PlayerView::loadPlayer()
+    {
+        calculatePlayerDimensions();
+        initializePlayerImage();
+    }
+
+    void PlayerView::initialize()
+    {
+        game_window = ServiceLocator::getInstance()->getGraphicService()->getGameWindow();
+        loadPlayer();
+    }
+
+    sf::Vector2f PlayerView::calulcatePlayerPosition()
+    {
+        return sf::Vector2f(0, 0);
+    }
+
+    void PlayerView::updatePlayerPosition()
+    {
+        player_image->setPosition(calulcatePlayerPosition());
+    }
+
+    void PlayerView::drawPlayer()
+    {
+        player_image->render();
+    }
+
+}
