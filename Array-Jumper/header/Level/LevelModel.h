1b2d688c442e1228d6af5176ba06466ed67cd38e
Adnan Sayyed <adnan.sayyed.1947@gmail.com>
2024-10-31 13:01:27 +0530

    Drawn background image

---
+++ Array-Jumper/header/Level/LevelModel.h
@@ -0,0 +1,11 @@
+#pragma once
+
+
+namespace Level
+{
+	class LevelModel
+	{
+	public:
+	
+	};
+}
--- Array-Jumper/source/Graphics/GraphicService.cpp
+++ Array-Jumper/source/Graphics/GraphicService.cpp
@@ -1,6 +1,7 @@
 #include "../../header/Graphics/GraphicService.h"
 #include "../../header/Global/Config.h"
 
+#include <iostream>
 
 namespace Graphics
 {
@@ -28,9 +29,59 @@ namespace Graphics
 
 	void GraphicService::configureVideoMode()
 	{
-		video_mode = *(new sf::VideoMode(game_window_width, game_window_height, sf::VideoMode::getDesktopMode().bitsPerPixel));
-	}
 
+		// Retrieve all supported fullscreen modes
+		// std::vector<sf::VideoMode> modes = sf::VideoMode::getFullscreenModes();
+
+		// sf::VideoMode selected_mode;
+		// bool valid_mode_found = false;
+
+		// // Loop through the modes to find the first valid one
+		// for (const auto &mode : modes)
+		// {
+		// 	if (mode.isValid())
+		// 	{
+		// 		selected_mode = mode;
+		// 		valid_mode_found = true;
+		// 		std::cout << "Selected Video Mode - Width: " << mode.width
+		// 				  << ", Height: " << mode.height
+		// 				  << ", Bits per Pixel: " << mode.bitsPerPixel << std::endl;
+		// 		break;
+		// 	}
+		// }
+
+		// if (valid_mode_found)
+		// {
+		// 	// Create a window using the valid mode
+		// 	sf::RenderWindow window(selected_mode, "Selected Fullscreen Mode", sf::Style::Fullscreen);
+
+		// 	// Main loop (example)
+		// 	while (window.isOpen())
+		// 	{
+		// 		sf::Event event;
+		// 		while (window.pollEvent(event))
+		// 		{
+		// 			if (event.type == sf::Event::Closed)
+		// 			{
+		// 				window.close();
+		// 			}
+		// 		}
+
+		// 		window.clear();
+		// 		// Draw stuff here
+		// 		window.display();
+		// 	}
+		// }
+		// else
+		// {
+		// 	std::cout << "No valid fullscreen mode available!" << std::endl;
+		// }
+
+		//video_mode = *(new sf::VideoMode(selected_mode.width, selected_mode.height, selected_mode.bitsPerPixel));
+
+		 video_mode = *(new sf::VideoMode(game_window_width, game_window_height, sf::VideoMode::getDesktopMode().bitsPerPixel));
+	}
+	
 	void GraphicService::setFrameRate(int frame_rate_to_set) { game_window->setFramerateLimit(frame_rate_to_set); }
 
 	bool GraphicService::isGameWindowOpen() { return game_window->isOpen(); }
